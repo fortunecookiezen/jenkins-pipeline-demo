0a8480ecf8df48bd5fed946302688564a6f67e5b
jamesp <phillips.james@gmail.com>
2019-11-02 14:45:45 -0500

    adding go test

--- tests/snsTest.sh
+++ tests/snsTest.sh
@@ -3,4 +3,6 @@ topic=$(terraform output topic_arn)
 echo $topic "is the topic arn"
 apk add --no-cache musl-dev go
 go get -u github.com/aws/aws-sdk-go/...
-CGO_ENABLED=0 go build tests/*.go
+CGO_ENABLED=0 go build -o snsTest tests/snsTest.go
+ls -l snsTopic
+#./snsTest -e "phillips.james@gmail.com" -a $topic
