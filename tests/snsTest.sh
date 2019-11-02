e88f7dfb967cb21ddb15c0a2eec49e7ce038815a
jamesp <phillips.james@gmail.com>
2019-11-02 17:38:41 -0500

    adding go test

--- tests/snsTest.sh
+++ tests/snsTest.sh
@@ -5,5 +5,5 @@ apk add --no-cache musl-dev go
 go get -u github.com/aws/aws-sdk-go/...
 CGO_ENABLED=0 go build -o snsTest tests/snsTest.go
 ls -l snsTest
-#./snsTest -e "phillips.james@gmail.com" -t $topic
-echo "access key is" $AWS_ACCESS_KEY_ID
+./snsTest -e "phillips.james@gmail.com" -t $topic
+# echo "access key is" $AWS_ACCESS_KEY_ID
