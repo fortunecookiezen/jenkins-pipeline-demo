61d02a1b3904392ce06d61e7971d22b6d793f515
jamesp <phillips.james@gmail.com>
2019-11-02 14:52:41 -0500

    adding go test

--- tests/snsTest.sh
+++ tests/snsTest.sh
@@ -4,5 +4,5 @@ echo $topic "is the topic arn"
 apk add --no-cache musl-dev go
 go get -u github.com/aws/aws-sdk-go/...
 CGO_ENABLED=0 go build -o snsTest tests/snsTest.go
-ls -l snsTopic
-#./snsTest -e "phillips.james@gmail.com" -a $topic
+ls -l snsTest
+#./snsTest -e "phillips.james@gmail.com" -t $topic
