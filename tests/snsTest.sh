20ce5956e93bc82d13da4ecb2f13332a3b5fdc71
jamesp <phillips.james@gmail.com>
2019-11-02 13:55:52 -0500

    trying to squash a go error

--- tests/snsTest.sh
+++ tests/snsTest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 topic=$(terraform output topic_arn)
 echo $topic "is the topic arn"
-apk add --no-cache make gcc build-base musl-dev go
+apk add --no-cache musl-dev go
 CGO_ENABLED=0 go run tests/*.go
