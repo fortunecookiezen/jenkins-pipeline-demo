71ad2c35951a55d1277ebf7ab81c6fa61825cba4
jamesp <phillips.james@gmail.com>
2019-11-02 17:43:35 -0500

    adding go test

--- tests/snsTest.sh
+++ tests/snsTest.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-topic=$(terraform output topic_arn)
-echo $topic "is the topic arn"
 apk add --no-cache musl-dev go
 go get -u github.com/aws/aws-sdk-go/...
 CGO_ENABLED=0 go build -o snsTest tests/snsTest.go
-ls -l snsTest
+topic=$(terraform output topic_arn)
 ./snsTest -e "phillips.james@gmail.com" -t $topic
-# echo "access key is" $AWS_ACCESS_KEY_ID
