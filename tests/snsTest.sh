a8085802d44e286b19c5077bf6c6158e38f743a3
jamesp <phillips.james@gmail.com>
2019-11-02 13:52:02 -0500

    adding go test

--- tests/snsTest.sh
+++ tests/snsTest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 topic=$(terraform output topic_arn)
 echo $topic "is the topic arn"
-apk add --no-cache make musl-dev go
+apk add --no-cache make gcc build-base musl-dev go
 go run tests/*.go
